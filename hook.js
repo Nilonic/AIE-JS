45249250864fb8ad9f249a150c2c7ccf06beb3a0
Nilonic <aaaaaa1243999999@gmail.com>
2025-01-28 20:56:49 +0000

    deob non-min, because i'm an idiot

--- hook.js
+++ hook.js
@@ -4,9 +4,9 @@
     console.log("Any issues with AIE.JS, report them here: https://github.com/Nilonic/AIE-JS/issues");
     let shouldBlockkeydown = false;
     let shouldBlockRightClick = false;
-    document.addEventListener("\x44\x4F\x4D\x43\x6F\x6E\x74\x65\x6E\x74\x4C\x6F\x61\x64\x65\x64", () => {
+    document.addEventListener("DOMContentLoaded", () => {
         try {
-            let ev = document.getElementById("\x41\x49\x53\x45\x2D\x43\x4F\x4E\x46\x49\x47");
+            let ev = document.getElementById("AISE-CONFIG");
             try {
 
                 if (ev.getAttribute("kd") == "1") {
@@ -26,24 +26,24 @@
             } catch {
                 shouldBlockRightClick = false;
             }
-            document.getElementById("\x41\x49\x53\x45\x2D\x43\x4F\x4E\x46\x49\x47").remove();
+            document.getElementById("AISE-CONFIG").remove();
         } catch {
 
         }
         (() => {
-            document.addEventListener("\x6B\x65\x79\x64\x6F\x77\x6E", (ev) => { // keydown
+            document.addEventListener("keydown", (ev) => { // keydown
                 if (shouldBlockkeydown) {
                     if (ev.ctrlKey && ev.shiftKey && ev.key === "\x49") {
                         ev.preventDefault();
                     }
-                    if (ev.code === "\x46\x31\x32") {
+                    if (ev.code === "F12") {
                         ev.preventDefault();
                     } else {}
                 }
             });
         })();
         (() => {
-            document.addEventListener("\x63\x6F\x6E\x74\x65\x78\x74\x6D\x65\x6E\x75", (ev) => { // context menu
+            document.addEventListener("contextmenu", (ev) => { // context menu
                 if (shouldBlockRightClick) {
                     ev.preventDefault();
                 }
